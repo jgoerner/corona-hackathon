2163fe4ae02e14474a5d2c887e88f157a68b54b6
jgoerner <joshua.goerner@gmail.com>
2020-03-21 19:51:15 +0100

    Include Employee Mock Controller

---
+++ src/main/java/io/swag/corona/employee/adapter/in/EmployeeWebController.java
@@ -0,0 +1,52 @@
+package io.swag.corona.employee.adapter.in;
+
+import io.swag.corona.employee.application.port.in.CreateEmployeeUseCase;
+import io.swag.corona.employee.application.port.in.DeleteEmployeeUseCase;
+import io.swag.corona.employee.application.port.in.GetEmployeeUseCase;
+import io.swag.corona.employee.application.port.in.UpdateEmployeeUseCase;
+import io.swag.corona.employee.domain.Employee;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class EmployeeWebController {
+
+    private final CreateEmployeeUseCase createEmployeeUseCase;
+    private final DeleteEmployeeUseCase deleteEmployeeUseCase;
+    private final GetEmployeeUseCase getEmployeeUseCase;
+    private UpdateEmployeeUseCase updateEmployeeUseCase;
+
+    @RequestMapping(path = "/employee", method = RequestMethod.POST)
+    Employee create(
+            @RequestParam("name") String name,
+            @RequestParam("ageGroup") String ageGroup,
+            @RequestParam("location") String location,
+            @RequestParam("skills") boolean[] skills
+    ) {
+        return createEmployeeUseCase.create(name, ageGroup, location, skills);
+    }
+
+    @RequestMapping(path = "/employee/{id}", method = RequestMethod.GET)
+    Employee getById(@PathVariable("id") String id) {
+        return getEmployeeUseCase.getById(id);
+    }
+
+    @RequestMapping(path = "/employee/{id}", method = RequestMethod.PUT)
+    Employee update(
+            @PathVariable("id") String id,
+            @RequestParam("name") String name,
+            @RequestParam("ageGroup") String ageGroup,
+            @RequestParam("location") String location,
+            @RequestParam("skills") boolean[] skills
+    ) {
+        return updateEmployeeUseCase.updateEmployee(id, name, ageGroup, location, skills);
+    }
+
+    @RequestMapping(path = "/employee/{id}", method = RequestMethod.DELETE)
+    void deleteById(@PathVariable("id") String id) {
+        deleteEmployeeUseCase.deleteById(id);
+    }
+
+}
---
+++ src/main/java/io/swag/corona/employee/application/service/EmployeeMockService.java
@@ -0,0 +1,67 @@
+package io.swag.corona.employee.application.service;
+
+import com.github.javafaker.Faker;
+import io.swag.corona.employee.application.port.in.CreateEmployeeUseCase;
+import io.swag.corona.employee.application.port.in.DeleteEmployeeUseCase;
+import io.swag.corona.employee.application.port.in.GetEmployeeUseCase;
+import io.swag.corona.employee.application.port.in.UpdateEmployeeUseCase;
+import io.swag.corona.employee.domain.Employee;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class EmployeeMockService implements CreateEmployeeUseCase, DeleteEmployeeUseCase, GetEmployeeUseCase, UpdateEmployeeUseCase {
+
+    private Faker faker;
+
+    @PostConstruct
+    void init(){
+        this.faker = new Faker();
+    }
+
+    @Override
+    public Employee create(String name, String ageGroup, String location, boolean[] skills) {
+
+        return new Employee(
+                faker.bothify("????-????-????-????"),
+                name,
+                ageGroup,
+                location,
+                skills);
+    }
+
+    @Override
+    public void deleteById(String id) {
+        // do nothing :-)
+    }
+
+    @Override
+    public Employee getById(String id) {
+
+        boolean[] skills = new boolean[5];
+        for(int i = 0; i < 5; i++) {
+            skills[i] = faker.bool().bool();
+        }
+
+        return new Employee(
+                id,
+                faker.name().toString(),
+                faker.numerify("??-??"),
+                faker.numerify("?????"),
+                skills);
+    }
+
+    @Override
+    public Employee updateEmployee(String id, String name, String ageGroup, String location, boolean[] skills) {
+
+        return new Employee(
+                id,
+                name,
+                ageGroup,
+                location,
+                skills
+        );
+
+    }
+}
