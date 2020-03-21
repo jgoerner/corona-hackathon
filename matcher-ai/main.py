ce4907a2c2ef23507ef88051b21d0599646cf3b2
Lorena Schlesinger <lorena.schlesinger@online.de>
2020-03-21 10:48:56 +0100

    [matcher-api] Refine initial API

--- matcher-ai/main.py
+++ matcher-ai/main.py
@@ -6,8 +6,10 @@ if config_name is None:
     config_name = "development"
 app = matcher.create_app(config_name)
 
+
 def main():
     app.run(host="127.0.0.1", port=8000, debug=True)
+
 
 if __name__ == '__main__':
     main()
--- matcher-ai/matcher/api.py
+++ matcher-ai/matcher/api.py
@@ -1,9 +1,30 @@
 from flask import Blueprint, make_response, jsonify, request
 
-api = Blueprint("matcherapi", __name__)
+api = Blueprint("matcher-api", __name__)
 
-@api.route("/", methods=["GET"])
-def root():
-    query = request.args.get(key="query", default="")
-    results = jsonify([1,2,3])
+
+@api.route("/special-demand", methods=["GET"])
+def special_demand():
+    """
+    Special demand request with zip code
+
+    :return: List[str] with skills
+    """
+    zip_code = request.args.get(key="zip_code", default="")
+    # TODO: get skills required in zip code area
+    results = jsonify([])
+    return make_response(results), 200
+
+
+@api.route("/recommendations", methods=["GET"])
+def recommendations():
+    """
+    Get prioritized job list for employee
+
+    :return: List[{job_id, score, explanation}] with recommendations
+    """
+    employee_id = request.args.get(key="employee_id", default="")
+    print(employee_id)
+    # TODO: get job recommendations from engine for employee
+    results = jsonify([])
     return make_response(results), 200
