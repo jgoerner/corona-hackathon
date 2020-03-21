ce5229bb8c8f26951bb26b4cd555519fb8aeeeae
Daniel Elsner <daniel.elsner@tum.de>
2020-03-21 10:25:35 +0100

    init python project structure

---
+++ matcher-ai/main.py
@@ -0,0 +1,13 @@
+import matcher
+import os
+
+config_name = os.getenv('APP_SETTINGS')
+if config_name is None:
+    config_name = "development"
+app = matcher.create_app(config_name)
+
+def main():
+    app.run(host="127.0.0.1", port=8000, debug=True)
+
+if __name__ == '__main__':
+    main()
---
+++ matcher-ai/tests/test_api.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestApi(unittest.TestCase):
+
+    def test_true(self):
+        self.assertTrue(True)
+
+
