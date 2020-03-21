63ab593df7c3875e2fce1180d0bcc14cfa3c34f8
jgoerner <joshua.goerner@gmail.com>
2020-03-21 20:15:55 +0100

    Implement Employer Mock Controller

---
+++ src/main/java/io/swag/corona/employer/adapter/in/web/EmployerWebController.java
@@ -0,0 +1,40 @@
+package io.swag.corona.employer.adapter.in.web;
+
+import io.swag.corona.employer.application.port.in.CreateEmployerUseCase;
+import io.swag.corona.employer.application.port.in.GetEmployerUseCase;
+import io.swag.corona.employer.application.port.in.UpdateEmployerUseCase;
+import io.swag.corona.employer.domain.Employer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class EmployerWebController {
+
+    private final CreateEmployerUseCase createEmployerUseCase;
+    private final GetEmployerUseCase getEmployerUseCase;
+    private final UpdateEmployerUseCase updateEmployerUseCase;
+
+    @RequestMapping(path = "/employer", method = RequestMethod.POST)
+    Employer create(
+            @RequestParam("name") String name,
+            @RequestParam("domain") String domain,
+            @RequestParam("homepage") String homepage) {
+        return createEmployerUseCase.create(name, domain, homepage);
+    }
+
+    @RequestMapping(path = "/employer/{id}", method = RequestMethod.GET)
+    Employer getById(@PathVariable("id") String id){
+        return getEmployerUseCase.getById(id);
+    }
+
+    @RequestMapping(path = "/employer/{id}", method = RequestMethod.PUT)
+    Employer update(
+            @PathVariable("id") String id,
+            @RequestParam("name") String name,
+            @RequestParam("domain") String domain,
+            @RequestParam("homepage") String homepage) {
+        return updateEmployerUseCase.update(id, name, domain, homepage);
+    }
+}
--- src/main/java/io/swag/corona/employer/application/service/MockService.java
+++ src/main/java/io/swag/corona/employer/application/service/MockService.java
@@ -1,14 +1,48 @@
 package io.swag.corona.employer.application.service;
 
+import com.github.javafaker.Faker;
 import io.swag.corona.employer.application.port.in.CreateEmployerUseCase;
+import io.swag.corona.employer.application.port.in.GetEmployerUseCase;
+import io.swag.corona.employer.application.port.in.UpdateEmployerUseCase;
 import io.swag.corona.employer.domain.Employer;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Component
-public class MockService implements CreateEmployerUseCase {
+public class MockService implements CreateEmployerUseCase, GetEmployerUseCase, UpdateEmployerUseCase {
+
+    private Faker faker;
+
+    @PostConstruct
+    void init() {
+        this.faker = new Faker();
+    }
 
     @Override
     public Employer create(String name, String domain, String homepage) {
-        return new Employer("ABCD", "ALDI", "Einzelhandel", "http://aldi.de");
+        return new Employer(
+                faker.bothify("????-????-????-????"),
+                name,
+                domain,
+                homepage);
+    }
+
+    @Override
+    public Employer getById(String employerId) {
+        return new Employer(
+                employerId,
+                faker.name().name(),
+                faker.commerce().department(),
+                faker.internet().domainName());
+    }
+
+    @Override
+    public Employer update(String id, String name, String domain, String homepage) {
+        return new Employer(
+                id,
+                name,
+                domain,
+                homepage);
     }
 }
