0ca11a51f8290a12dc794d7e101e49049d82f1fe
Lorena Schlesinger <lorena.schlesinger@online.de>
2020-03-22 12:15:48 +0100

    More templates filled

--- frontend/src/app/pages/swag/find/employee-profile/employee-profile.component.ts
+++ frontend/src/app/pages/swag/find/employee-profile/employee-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-employee-profile',
@@ -6,18 +7,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-profile.component.scss']
 })
 export class EmployeeProfileComponent implements OnInit {
+  validateForm: FormGroup;
 
-  index = 0;
-  disable = false;
-  size = 'large';
+  submitForm(): void {
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+    }
+  }
+  current = 0;
 
-  onIndexChange(index: number): void {
-    this.index = index;
+  index = 'Give information about age group, location and possible start time';
+
+  pre(): void {
+    this.current -= 1;
+    this.changeContent();
   }
 
-  constructor() { }
-
-  ngOnInit() {
+  next(): void {
+    this.current += 1;
+    this.changeContent();
   }
 
+  done(): void {
+    console.log('done');
+  }
+
+  changeContent(): void {
+    switch (this.current) {
+      case 0: {
+        this.index = 'Give information about age group, location and possible start time';
+        break;
+      }
+      case 1: {
+        this.index = 'Describe your skill set';
+        break;
+      }
+      case 2: {
+        this.index = 'Ready to save or trigger matching';
+        break;
+      }
+      default: {
+        this.index = 'error';
+      }
+    }
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      age: [null, [Validators.required]],
+      location: [null, [Validators.required]]
+  });
+  }
+
+  dateMode = 'time';
+
+  handleDateOpenChange(open: boolean): void {
+    if (open) {
+      this.dateMode = 'time';
+    }
+  }
+
+  handleDatePanelChange(mode: string): void {
+    console.log('handleDatePanelChange: ', mode);
+  }
 }
