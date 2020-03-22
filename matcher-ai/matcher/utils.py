dceb1d579bf074652182692e1e71cccdccf50466
Floris Roltsch <Floris.Roltsch@TotalAzureCloud.onmicrosoft.com>
2020-03-22 18:38:27 +0100

    Cutting-edge AI recommendation engine

--- matcher-ai/matcher/utils.py
+++ matcher-ai/matcher/utils.py
@@ -1,42 +1,69 @@
 import pgeocode
+import pandas as pd
+import random
 
-def filter_and_score_jobs(df_employee, df_jobs):
+random.seed = 1
+
+
+def filter_and_score_jobs(employee_id):
     """
-    Function that filters possible jobs for one employee by hard criteria (student, german speaking, drivers-license),
-    scores jobs by soft-criteria (working hours, start date, distance)
+    Function that filters possible jobs for one employee by hard criteria (student, german speaking, drivers-license,
+    working hours), scores jobs by soft-criteria (start date, distance)
     returns a df with all possible jobs for this employee and the respective scores
     """
-    df_employee_job_matching = pd.merge(df_jobs, df_employee, how='left', left_index=True, right_index=True).ffill()
+    # connections string
+    con = "postgres://fhxagpklsgbryv:24e07502a507c1d486bc46b3eceb0bec570b2810ac15efe2f3d1e0fdffedd750@ec2-54-217-" \
+          "204-34.eu-west-1.compute.amazonaws.com:5432/d6n76bgdm2kuf3"
+    zip_code_list = [80796, 80798, 80333, 80538, 80939, 80331, 81245, 80803, 80809, 80999]
 
-    hard_skills_col_dict = {'german_speaking': 'german_required', 'drivers_license':'drivers_license_required',
-                            'student':'student_job'}
+    # get metadata for employee
+    df_employee = pd.read_sql("SELECT * FROM employee WHERE id = '{}'".format(employee_id), con=con)
+    # mock employee zip code
+    df_employee['zip_code'] = str(random.choice(zip_code_list))
+    # mock english speaking
+    df_employee['english_speaking'] = random.choice([True, False])
+
+    # load metadata for jobs that still have demand
+    # df_jobs = pd.read_sql_table("job", con=con)
+    df_jobs = pd.read_sql("SELECT * FROM job WHERE current_demand_qty > 0", con=con)
+    df_jobs = df_jobs[df_jobs['current_demand_qty']>0]
+    # mock job zip code
+    df_jobs['zip_code'] = df_jobs.apply(lambda x: str(random.choice(zip_code_list)), axis=1)
+    df_jobs['student_job'] = df_jobs.apply(lambda x: random.choice([True, False]), axis=1)
+    # mock english requirement
+    df_jobs['english_required'] = df_jobs.apply(lambda x: random.choice([True, False]), axis=1)
+
+    hard_skills_col_dict = {'german_speaking': 'german_required', 'english_speaking': 'english_required',
+                            'drivers_license': 'drivers_license_required', 'student': 'student_job'}
     for skill, requirement in hard_skills_col_dict.items():
-        if df_employee[skill]==False:
-            df_employee_job_matching = df_employee_job_matching.loc[df_employee_job_matching[requirement]==False]
-
-     # filter if working hours > max working hours o employee
-     df_employee_job_matching = df_employee_job_matching.loc[df_employee_job_matching['working_hours'] <= df_employee_job_matching['working_hours']]
+            if not df_employee[skill].iloc[0]:
+                df_jobs = df_jobs.loc[~(df_jobs[requirement]==True)]
+    # filter if working hours > max working hours of employee
+    df_jobs = df_jobs.loc[df_jobs['hours_per_week'] <= df_employee['max_h_per_week'].iloc[0]]
 
     # TODO filter jobs that start too late
     # calculate distance between zip codes
+    dist = pgeocode.GeoDistance('DE')
+    df_jobs['distance'] = df_jobs.apply(
+        lambda x: dist.query_postal_code(df_employee['zip_code'].iloc[0], x['zip_code']), axis=1)
+    if df_jobs['distance'].max() != df_jobs['distance'].min():
+        df_jobs['distance_score'] = (df_jobs['distance'].max() - df_jobs['distance']) / \
+                                                     (df_jobs['distance'].max() - df_jobs['distance'].min())
+    else:
+        df_jobs['distance_score'] = 1
 
-     dist = pgeocode.GeoDistance('DE')
-     df_employee_job_matching['distance'] = dist.query_postal_code(df_employee_job_matching['employee_zip_code'],
-                                                df_employee_job_matching['job_zip_code'])
+    # score for salary (max salary =1, min salary = 0)
+    if df_jobs['salary_per_h'].max() != df_jobs['salary_per_h'].min():
+        df_jobs['salary_score'] = (df_jobs['salary_per_h'].max() - df_jobs['salary_per_h']) / \
+                                                     (df_jobs['salary_per_h'].max() - df_jobs['salary_per_h'].min())
+    else:
+        df_jobs['salary_score'] = 1
 
-    return df_employee_job_matching.sort_values('distance')
+    # average of scores
+    df_jobs['score'] = df_jobs[['distance_score', 'salary_score']].mean(axis=1).round(2)
 
-def score_jobs(df_jobs_filtered):
+    # TODO: add explanation
+    df_jobs['explanation'] = 'Beste Leben'
+    df_jobs['jobId'] = df_jobs['id']
 
-df_employee = pd.DataFrame([['a'], [True], [True], [False], [80796], [40]], columns = ['employee_id', 'german_speaking',
-                                                                                       'drivers_license', 'student',
-                                                                                       'employee_zip_code',
-                                                                                       'max_working_hours'])
-df_job = pd.DataFrame([['e', 'f', 'g'], [True, False, True], [True, True, True], [False, False, True],
-                                                                                      [80796, 80333, 20149],
-                                                                                      [40, 40, 40]],
-                                                                                        columns = ['job_id',
-                                                                                                   'german_required',
-                                                                                       'drivers_license', 'student',
-                                                                                       'job_zip_code',
-                                                                                       'working_hours'] )
+    return df_jobs.sort_values('score', ascending=False)[['explanation', 'score', 'jobId']].head(3).to_dict('records')
